958b13512253aacd6e614bdf1a08946661cccd0b
Jean-baptiste Aujogue <jb.aujogue@gmail.com>
2025-07-13 23:55:25 +0200

    change display function params

--- src/rubik/display.py
+++ src/rubik/display.py
@@ -1,13 +1,16 @@
-def stringify(cube) -> str:
+import torch
+
+
+def stringify(state: torch.Tensor, colors: list[str], size: int) -> str:
     """
     Compute a string representation of a cube.
     """
-    colors = pad_colors(cube.colors)
-    faces = cube.state.reshape(6, cube.size, cube.size).transpose(1, 2)
+    colors = pad_colors(colors)
+    faces = state.reshape(6, size, size).transpose(1, 2)
     faces = [[[colors[i - 1] for i in row] for row in face.tolist()] for face in faces]
-    space = " " * max(len(c) for c in cube.colors) * cube.size
+    space = " " * max(len(c) for c in colors) * size
     l1 = "\n".join(" ".join([space, "".join(row), space, space]) for row in faces[0])
-    l2 = "\n".join(" ".join("".join(face[i]) for face in faces[1:5]) for i in range(cube.size))
+    l2 = "\n".join(" ".join("".join(face[i]) for face in faces[1:5]) for i in range(size))
     l3 = "\n".join(" ".join((space, "".join(row), space, space)) for row in faces[-1])
     return "\n".join([l1, l2, l3])
 
